36b0b7d357d6e0b0870bef92a7bcc462cc5d56a7
NVillalpando <nvillalpando@cpp.edu>
2021-03-25 13:13:24 -0700

    fixed a compile error in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import react from 'react'
+import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
