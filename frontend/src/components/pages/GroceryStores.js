5275c72a9170924577b5777fb1471c5bdde487fa
ricardoaxelbaeza <ricardobaeza2000@gmail.com>
2021-03-25 16:38:37 -0700

    attempt to display data on Grocery Stores Tab

--- frontend/src/components/pages/GroceryStores.js
+++ frontend/src/components/pages/GroceryStores.js
@@ -1,50 +1,61 @@
 import React from 'react';
-// import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
-// import { List, Avatar, Space } from 'antd';
+import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+import { List, Avatar, Space } from 'antd';
+
+
+
+const IconText = ({ icon, text }) => (
+  <Space>
+    {React.createElement(icon)}
+    {text}
+  </Space>
+);
+
 
 const GroceryStores = (props) => {
     return(
-      <h1>Grocery Stores</h1>
-  // <List
-  //   itemLayout="vertical"
-  //   size="large"
-  //   pagination={{
-  //     onChange: page => {
-  //       console.log(page);
-  //     },
-  //     pageSize: 3,
-  //   }}
-  //   dataSource={listData}
-  //   footer={
-  //     <div>
-  //       <b>ant design</b> footer part
-  //     </div>
-  //   }
-  //   renderItem={item => (
-  //     <List.Item
-  //       key={item.title}
-  //       actions={[
-  //         <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-  //         <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-  //         <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-  //       ]}
-  //       extra={
-  //         <img
-  //           width={272}
-  //           alt="logo"
-  //           src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-  //         />
-  //       }
-  //     >
-  //       <List.Item.Meta
-  //         avatar={<Avatar src={item.avatar} />}
-  //         title={<a href={item.href}>{item.title}</a>}
-  //         description={item.description}
-  //       />
-  //       {item.content}
-  //     </List.Item>
-  //   )}
-  // />
+      // <h1>Grocery Stores</h1>
+
+        <List
+          itemLayout="vertical"
+          size="large"
+          pagination={{
+            onChange: page => {
+              console.log(page);
+            },
+            pageSize: 3,
+          }}
+          dataSource={props.data}
+          footer={
+            <div>
+              <b>ant design</b> footer part
+            </div>
+          }
+          renderItem={item => (
+            <List.Item
+              key={item.title}
+              actions={[
+                <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+              ]}
+              extra={
+                <img
+                  width={272}
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                />
+              }
+            >
+              <List.Item.Meta
+                avatar={<Avatar src={item.avatar} />}
+                title={<a href={item.href}>{item.title}</a>}
+                description={item.description}
+              />
+              {item.content}
+            </List.Item>
+          )}
+        />
     )
 }
 
