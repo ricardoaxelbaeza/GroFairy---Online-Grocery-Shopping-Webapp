857dac7143f244cdf6897839ce9ae7e26a8eb1ee
ricardoaxelbaeza <ricardobaeza2000@gmail.com>
2021-03-24 23:57:56 -0700

    changes to display data on grocery stores section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
-
-
+import 'antd/dist/antd.css'; 
+import GroceryStores from './components/GroceryStores';
 
 
 
@@ -15,6 +15,7 @@ function App() {
        <Navbar/>
         <Switch>
          <Route path='/' exact component={Home}/>
+         <Route path='/grocerystores' exact component={GroceryStores}/>
         </Switch>
       </Router>
     </>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>,
+ document.getElementById
   ('root'));
+
 
 
