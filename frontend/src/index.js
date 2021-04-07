f21264650c5d2982d827c10b87a93ab24bcac732
NVillalpando <nvillalpando@cpp.edu>
2021-04-07 16:23:11 -0700

    grocery store bug fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter className>
+    <BrowserRouter>
         <App />
     </BrowserRouter>,
     document.getElementById
