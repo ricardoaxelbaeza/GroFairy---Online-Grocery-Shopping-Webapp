53d1978f29f3e0227263401641c9e57570712fa4
ricardoaxelbaeza <ricardobaeza2000@gmail.com>
2021-04-08 16:38:47 -0700

    updates to home screen

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -5,14 +5,10 @@ import './HeroSection.css'
 
 export default function HeroSection() {
   return (
-<<<<<<< HEAD
-    <div className='hero-container' style ={{
-      backgroundImage: `url(${process.env.PUBLIC_URL + '/GroFairy3.jpeg'})` 
-     
-=======
+
     <div className='hero-container' style={{
-      backgroundColor: 'pink',
->>>>>>> 3a4f68a4f56e03ba7a6c4d1ea3ee02ecb3709a6c
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/GroFairy3.jpeg'})` 
+
     }}>
       {/* <img src={process.env.PUBLIC_URL + "/GroFairyLogo.jpeg"} width="1792" alt="GroFairy Logo" /> */}
       
