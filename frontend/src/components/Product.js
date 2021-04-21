7362d2dcb508fe857d6b7799b971e6aa3997864a
y-shinkawa <68253502+y-shinkawa@users.noreply.github.com>
2021-04-21 12:34:14 -0700

    Add files via upload

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography } from '@material-ui/core'
 import makeStyles from './GroceryStyles'
+import "./ProductScreen.css";
+import { AddToCart } from './actions/CartActions';
 
 const product = ({ product }) => {
     const classes = makeStyles();
@@ -16,9 +18,25 @@ const product = ({ product }) => {
                         <Typography variant='h5'>
                             ${product.unit_price}
                         </Typography>
+                        <Typography variant='h5' color ="textSecondary">{product.description}</Typography>
                     </div><Typography variant='body2' color='textSecondary'>
-                        Available Stock: {product.stock}
+                        <p>
+                            Status: {" "}
+                            <span>
+                                {product.stock > 0 ? "In stock" : "Out of stock"}
+                            </span>
+                        </p>
+                        <p>
+                            Qty
+                            <select>
+                                <option value = "1">1</option>
+                                <option value = "2">2</option>
+                                <option value = "3">3</option>
+                                <option value = "4">4</option>
+                            </select>
+                        
                         <button>Add To Cart</button>
+                        </p>
                     </Typography>
                     {/*  */}
                 </CardContent>
@@ -30,3 +48,10 @@ const product = ({ product }) => {
 }
 
 export default product
+/*
+<select value = {qty} onChange{(e) => setQty(e.target.value)}> this is gonna be used to get how many of the product they want to buy
+    {[...Array(product.stock).keys()].map((x) => {
+        <option> key={x+1} value={x+1}>{x+1}</option>
+    })}
+</select>
+*/
---
+++ frontend/src/components/actions/CartActions.js
@@ -0,0 +1,30 @@
+import * as actionTypes from '../constants/CartConstants';
+import axios from 'axios';
+
+/*export const AddToCart = (id, qty) => async (dispatch, getState) => {
+    const {data} = await axios.get(`product.id`);
+
+    dispatch({
+        type: actionTypes.ADD_TO_CART,
+        payload: {
+            product: data.product_id,
+            name: data.product_name,
+            image: data.product_image,
+            price: data.unit_price,
+            stock: data.stock,
+            qty
+        }
+    })
+
+    localStorage.setItem('cart', JSON.stringify(getState().cart))
+};
+
+export const RemoveFromCart = () => (dispatch, getState) => {
+    dispatch({
+        type: actionTypes.REMOVE_FROM_CART,
+        payload: id
+    });
+
+    localStorage.setItem("cart", JSON.stringify(getState().cart.CartItems));
+};
+*/
