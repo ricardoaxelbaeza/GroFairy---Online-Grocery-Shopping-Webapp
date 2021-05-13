f48f0e47c6f71f44c92471366387fadba84f0762
NVillalpando <nvillalpando@cpp.edu>
2021-05-12 22:53:35 -0700

    shopping cart design revival

--- frontend/src/components/pages/ShoppingCart.js
+++ frontend/src/components/pages/ShoppingCart.js
@@ -1,7 +1,7 @@
 import '../../App.css'
 // import HeroSection from '../HeroSection'
 import "./ShoppingCart.css";
-// import CartItem from "./CartItem";
+import CartItem from "./CartItem";
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { Grid, Typography } from '@material-ui/core'
@@ -10,20 +10,33 @@ import { Grid, Typography } from '@material-ui/core'
 
 const ShoppingCart = () => {
     const [cart, setCart] = useState([]);
-  
+
     useEffect(() => {
-      fetch('http://54.151.124.251:8000/shoppingcart/?format=json')
-        .then(resp => resp.json())
-        .then(resp => {
-          console.log(resp)
-          setCart(resp)
-        })
-  
+        fetch('http://54.151.124.251:8000/shoppingcart/?format=json')
+            .then(resp => resp.json())
+            .then(resp => {
+                console.log(resp)
+                setCart(resp)
+            })
+
     }, [])
     return (
         <div className="shoppingcart" style={{ marginTop: '11vh' }}>
             <div className="shoppingcart_left">
-                <h2>Shopping cart</h2>
+                <CartItem />
+            </div>
+            <div className="shoppingcart_right">
+                <div className="shoppingcart_info" >
+                    <p>Subtotal (4) items</p>
+                    <p>$27.18</p>
+                </div>
+                <div>
+                    <button>
+                        <Link to='/checkout' style={{ color: 'white' }}>
+                            Proceed To Checkout
+                        </Link>
+                    </button>
+                </div>
             </div>
         </div>
     );
